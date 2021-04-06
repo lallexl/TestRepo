ed8d5da2ab5fd26ec5d81b614b79e305e46bf72b
lallexl <81842677+lallexl@users.noreply.github.com>
2021-04-06 18:14:32 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,3 +5,5 @@ systemctl restart docker.service && systemctl enable docker.service
 curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose #Ставим docker-compose на любую ОСь из поддерживаемых
 chmod +x /usr/bin/docker-compose #Делаем исполняемым
 docker --version && docker-compose --version # Проверяем версии
+
+find ./ -name "*.sh" -print -exec chmod u+x {} \;  #Делаем все баш скрипты в директории исполняемыми (для текущего пользователя)
