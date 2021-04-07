b4b192063c83ac30b77dac2cca061d01a5a627ca
lallexl <81842677+lallexl@users.noreply.github.com>
2021-04-07 11:07:04 +0700

    Update my-script.sh

--- my-script.sh
+++ my-script.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
+
+case $1 in
+    create | Create )
+        ./сreate_structure.sh;;
+    autoinsert | Autoinsert )
+        ./autoinsert.sh;;
+    'migrate_10' | "Migrate_10" )
+        ./migrate_10.sh;;
+    * )  echo "Параметр введен неверно"
+
 echo " "
 echo "Переменные для ввода: "
 echo "create - создаст структуру данных в базах"
 echo "autoinsert - заполнит данными первую бд PostgreSQL"
-echo "migrate 10 - перенсет данные с изменениями цены в 10 раз"
+echo "migrate_10 - перенсет данные с изменениями цены в 10 раз"
 echo " "
-
-echo -n "Введите параметр: "
-
-read param
-case $param in
-    create )
-        ./сreate_structure.sh;;
-    autoinsert )
-        ./autoinsert.sh;;
-    'migrate 10' )
-        ./migrate_10.sh;;
-    * )  echo "Параметр введен неверно";;
+    ;;
 esac
